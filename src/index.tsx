0b8f7b0842798b8cc109bcd5865132fb98897180
alex <arhontes@yandex.ru>
2023-01-05 19:16:36 +0300

    feature(index.tsx): implement react-router-dom, wrap App with BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './features/app/App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import {store} from "./features/app/store";
+import {BrowserRouter} from "react-router-dom";
+import {App} from "./features/app/App";
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>,
-  </React.StrictMode>
+    <React.StrictMode>
+
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>,
+
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
