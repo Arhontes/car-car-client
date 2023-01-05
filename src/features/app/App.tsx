cb4bdaf4d041e47b03fc5a1ffa125ad7e42357ac
alex <arhontes@yandex.ru>
2023-01-05 19:17:47 +0300

    feature(App.tsx): add Routes

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import {Container} from "@mui/material";
+import {Route, Routes} from "react-router-dom";
+import Main from "../main/Main";
+import SignIn from "../auth/sign-in/SignIn";
 import SignUp from "../auth/sign-up/SignUp";
 
-function App() {
+export const App = () => {
     return (
-        <div className="App">
-            <SignUp/>
-        </div>
+
+        <Container fixed>
+            <Routes>
+                <Route index element={<Main/>}/>
+                <Route path={'login'} element={<SignIn/>}/>
+                <Route path={'register'} element={<SignUp/>}/>
+            </Routes>
+        </Container>
     );
 }
 
-export default App;
+
+
