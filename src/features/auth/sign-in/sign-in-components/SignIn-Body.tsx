8b4cc7eb52d2167aadbceacd5d4c179fd9477ffb
alex <arhontes@yandex.ru>
2023-01-06 19:54:30 +0300

    refactor(SignIn-Body): implement react-hook-form

--- src/features/auth/sign-in/sign-in-components/SignIn-Body.tsx
+++ src/features/auth/sign-in/sign-in-components/SignIn-Body.tsx
@@ -1,52 +1,51 @@
 import React from 'react';
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 import {Box} from "@mui/material";
 import SignInBodyLinks from "./SignIn-BodyLinks";
+import {SubmitHandler, useForm} from "react-hook-form";
+import FormTextField from "../../../../common/components/FormTextField";
+import {validation} from "../../../../common/constans/validation";
+
+type FormInput  = {
+    email: string
+    password: string
+}
 
 const SignInBody = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
 
+    const {control, handleSubmit, formState: {isValid}} = useForm<FormInput>({mode: "all"});
+
+    const onSubmit: SubmitHandler<FormInput> = data => {
+        console.log(data)
+    };
     return (
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Электронная почта"
-                name="email"
-                autoComplete="email"
-                autoFocus
-            />
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Пароль"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-            />
-            <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Запомнить меня"
-            />
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{mt: 1}}>
+
+            <FormTextField
+                            label={"Email"}
+                            defaultValue={""}
+                            name={"email"}
+                            rules={{
+                                required:"Поле обязательно к заполнению",
+                                pattern: {value:validation.email,message: "Некорректный email"}
+                            }}
+                            control={control as any}/>
+
+            <FormTextField
+                            label={"Password"}
+                            defaultValue={""}
+                            name={"password"}
+                            rules={{
+                                required:"Введите пароль",
+                            }}
+                            control={control as any}/>
+
             <Button
+                disabled={!isValid}
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{mt: 3, mb: 2}}
             >
                 Войти
             </Button>
