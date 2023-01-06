7b248ef4036a70b6b6c8026e9913c1e4353420ed
Alex <arhontes@yandex.ru>
2023-01-06 12:54:03 +0300

    feature(error-handle-util.ts) - add new util

---
+++ src/common/utils/error-handle-utils.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosError } from 'axios';
+import {AppDispatch} from "../../features/app/store";
+import {appActions} from "../../features/app/appSlice";
+
+
+export const handleServerNetworkError = (
+    e: Error | AxiosError<{ error: string }>,
+    dispatch: AppDispatch,
+): void => {
+    const err = e as Error | AxiosError<{ error: string }>;
+
+    dispatch( appActions.changeAppStatus('failed'));
+
+    if (axios.isAxiosError(err)) {
+        // @ts-ignore
+        const error = err.response?.data ? err.response.data.error : err.message;
+
+        dispatch(appActions.setAppError(error));
+    } else {
+        dispatch(appActions.setAppError(`Native error ${err.message}`));
+    }
+
+    setTimeout(()=>{
+        appActions.setAppError(null)
+    },2000)
+};
