68bbbf845760f99a8cdb34b737ce714a8837ed98
Alex <arhontes@yandex.ru>
2023-01-06 12:52:02 +0300

    refactor(app-slice.ts) - add reducers to app-slice

--- src/features/app/appSlice.ts
+++ src/features/app/appSlice.ts
@@ -1,25 +1,31 @@
 import type {PayloadAction} from '@reduxjs/toolkit'
 import {createSlice} from '@reduxjs/toolkit'
-import {AppStateType} from "../../common/types/app-types";
+import {AppStateType, AppStatusType} from "../../common/types/app-types";
 
 const initialState: AppStateType = {
-    isInitialize:false,
-    error:null,
-    status:"idle",
+    isInitialize: false,
+    error: null,
+    status: "idle",
 }
 
-export const authSlice = createSlice({
+export const appSlice = createSlice({
     name: 'app',
     initialState,
     reducers: {
-        // incrementByAmount: (state, action: PayloadAction<number>) => {
-        //     state.value += action.payload
-        // },
+        toggleIsInitialize: (state, action: PayloadAction<boolean>) => {
+            state.isInitialize = action.payload
+        },
+        setAppError: (state, action: PayloadAction<string|null>) => {
+            state.error = action.payload
+        },
+        changeAppStatus: (state, action: PayloadAction<AppStatusType>) => {
+            state.status = action.payload
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
-// export const {incrementByAmount} = authSlice.actions
+export const appActions = appSlice.actions
 
-export const appReducer = authSlice.reducer
+export const appReducer = appSlice.reducer
 
