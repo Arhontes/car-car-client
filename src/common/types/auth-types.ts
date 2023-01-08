caa70f568337a6570a25729c7be87dc485b8668e
alex <arhontes@yandex.ru>
2023-01-08 21:37:27 +0300

    refactor(auth-types.ts): add new types

--- src/common/types/auth-types.ts
+++ src/common/types/auth-types.ts
@@ -1,3 +1,16 @@
 import {UserType} from "./common-types";
 
 export type AuthStateType = { isAuth: boolean } & UserType
+
+export type RegisterDtoType = {
+    firstName:string
+    lastName:string
+    password:string
+    phone:string
+    email:string
+}
+
+export type LoginDtoType = {
+    email:string
+    password:string
+}
