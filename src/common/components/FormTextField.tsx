7b25b12ab87ade32e0e8ff79243570b4286aac76
alex <arhontes@yandex.ru>
2023-01-08 21:41:46 +0300

    refactor(app-selectors): update props for component

--- src/common/components/FormTextField.tsx
+++ src/common/components/FormTextField.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {HTMLInputTypeAttribute} from 'react';
 import {UseControllerProps} from "react-hook-form/dist/types/controller";
 import {Controller} from "react-hook-form";
 import TextField from "@mui/material/TextField";
 
+export type FormTextFieldType = {label?:string,type?:HTMLInputTypeAttribute} & UseControllerProps
 
-const FormTextField = (props:UseControllerProps&{label:string}) => {
+const FormTextField = (props:FormTextFieldType) => {
 
     return (
         <Controller
@@ -16,7 +17,9 @@ const FormTextField = (props:UseControllerProps&{label:string}) => {
                          field: { onChange, onBlur, value, name, ref },
                          fieldState: { error},
                      }) => (
+
                 <TextField
+                    type={props.type}
                     error={!!error}
                     onBlur={onBlur}
                     onChange={onChange}
