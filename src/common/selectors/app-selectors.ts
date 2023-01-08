7e3e420f6448b99fca6e86ed03afd0bda15cd763
alex <arhontes@yandex.ru>
2023-01-08 21:40:42 +0300

    feature(app-selectors): add new selector

--- src/common/selectors/app-selectors.ts
+++ src/common/selectors/app-selectors.ts
@@ -1,6 +1,9 @@
 import {RootState} from "../../features/app/store";
+import {AppStatusType} from "../types/app-types";
 
 export const getAppErrorMessage = (state:RootState):string|null=>{
     return state.app.error
 }
-
+export const getAppStatus  = (state:RootState):AppStatusType=>{
+    return state.app.status
+}
