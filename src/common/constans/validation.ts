fd6474c84e1008d3d96b845b04b413a5ad4a5b56
alex <arhontes@yandex.ru>
2023-01-08 21:45:48 +0300

    feature(validation.ts): add new validation patterns, add alert message constants, add validation help methods

--- src/common/constans/validation.ts
+++ src/common/constans/validation.ts
@@ -1,4 +1,25 @@
 export const validation = {
     email: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
-    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}/
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}/,
+    phone: /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/,
+
+}
+
+export const validationMessages = {
+    isRequired: "Поле обязательно для заполнения",
+    incorrectEmail: "Некорректный email",
+    incorrectPassword: "Пароль должен состоять из цифр, а так же строчных и заглавных букв латинского алфафита",
+    incorrectConfirmPassword:"Пароли не совпадают",
+}
+
+export const validationHelpers = {
+    getMaxLengthMessage(value:number){
+        return {value, message:`Длина поля больше ${value} символов`}
+    },
+    getMinLengthMessage(value:number){
+        return {value, message:`Длина поля меньше ${value} символов`}
+    },
+    confirmPasswords(pass:string, confirmPass:string){
+        return pass!==confirmPass? validationMessages.incorrectConfirmPassword : true
+    }
 }
