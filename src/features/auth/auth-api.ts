cd464a26a0e664e6357803ed6ce4d44fdfeb978f
alex <arhontes@yandex.ru>
2023-01-13 19:48:24 +0300

    feature(auth-api): add axios interceptors, new post request to log out

--- src/features/auth/auth-api.ts
+++ src/features/auth/auth-api.ts
@@ -1,51 +1,46 @@
 import axios from 'axios'
 import {LoginDtoType, LoginResponseType, RefreshResponseType, RegisterDtoType} from "../../common/types/auth-types";
-import {UserType} from "../../common/types/common-types";
 
 export const instance = axios.create({
     baseURL: 'http://localhost:5000/auth/',
-    withCredentials:true
+    withCredentials: true
 })
-// instance.interceptors.request.use((config)=>{
-//     config.headers = { ...config.headers } as AxiosHeaders;
-//     config.headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`);
-//
-//     return config
-// })
-
-// instance.interceptors.request.use((config)=>{
-//     return config
-// }, async (error)=>{
-//     const originalRequest = error.config
-//     try {
-//         if (error.response.status === 401 && error.config && !error.config._isRetry){
-//             originalRequest._isRetry = true
-//             const response = await authAPI.refresh()
-//             return instance.request(originalRequest)
-//         }
-//     }catch(error){
-//         console.log(error)
-//     }
-//     throw error
-// })
+instance.interceptors.request.use((config) => {
+        // @ts-ignore
+        config.headers.set() ['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+        return config;
+    },
+    error => {
+        console.log("in request")
+        return Promise.reject(error);
+    })
+instance.interceptors.response.use((config) => {
+    return config
+}, async (error) => {
+    try {
+        if (error.response.status === 401 && error.config.url !== "refresh") {
+            const response = await authAPI.refresh()
+            localStorage.setItem('token', response.data.access_token)
+            return instance.request(error.congig)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    throw error
+})
 
 export const authAPI = {
 
-    async register(registerDto:RegisterDtoType){
-        return await instance.post(`reg`,registerDto)
+    async register(registerDto: RegisterDtoType) {
+        return await instance.post(`reg`, registerDto)
     },
-    async login(loginDTO:LoginDtoType){
-        return await instance.post<LoginResponseType>(`login`,loginDTO)
-    },async authMe(){
-        return await instance.get<UserType>(`me`)
+    async login(loginDTO: LoginDtoType) {
+        return await instance.post<LoginResponseType>(`login`, loginDTO)
     },
-    async refresh(){
-        return await instance.post<RefreshResponseType>(`refresh`,{})
+    async refresh() {
+        return await instance.post<RefreshResponseType>(`refresh`, {})
     },
-    async logout(){
-
+    async logout() {
+        return await instance.post(`logout`)
     },
 }
-
-
-
