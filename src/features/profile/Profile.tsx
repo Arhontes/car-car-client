ba7916e99311af203bce51186ba1bc6c4241b703
alex <arhontes@yandex.ru>
2023-01-13 19:58:34 +0300

    feature(Profile.tsx): use Dialog window in instead of static component

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useAppSelector} from "../../common/hooks/useAppSelector";
 import {getProfileData} from "../../common/selectors/profile-selectors";
+import {checkIsAuth} from "../../common/selectors/auth-selectors";
+import {useNavigate} from "react-router-dom";
+import {Dialog, DialogContent} from "@mui/material";
+import {ProfileDialog} from "./ProfileDialog";
+
 
 const Profile = () => {
 
     const profile = useAppSelector(getProfileData) as any
 
-    const props = [] as any[]
+    const [open,setOpen] = useState(true)
 
+    const handleClose = ()=>{
+        setOpen(false)
+    }
+    const isAuth = useAppSelector(checkIsAuth)
+    const navigate = useNavigate()
+    useEffect(()=>{
+        !open && navigate("/")
+    },[open])
 
-    for (const property in profile) {
-        props.push(<div>{profile[property]}</div>)
+    if (!isAuth){
+        navigate('login')
     }
     return (
-        <div>
-            {props}
-            {/*<div>{profile.email}</div>
-            <div>{profile.phone}</div>
-            <div>{profile.lastname}</div>
-            <div>{profile.}</div>
-            <div>{profile.email}</div>*/}
-        </div>
+        <Dialog
+            onClose={handleClose}
+            open={open}
+            aria-labelledby="alert-dialog-profile"
+            aria-describedby="alert-dialog-profile-description">
+            <DialogContent>
+                <ProfileDialog {...profile}/>
+            </DialogContent>
+        </Dialog>
     );
 };
 
