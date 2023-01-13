a9c8cb476eb22eca9ceae7cdd2095af23a117767
alex <arhontes@yandex.ru>
2023-01-13 19:45:04 +0300

    refactor(App.tsx): add preloader

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,46 +1,65 @@
 import * as React from 'react';
-import {Container, LinearProgress} from "@mui/material";
+import {useEffect} from 'react';
+import {Container} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
-import Main from "../main/Main";
 import SignIn from "../auth/sign-in/SignIn";
 import SignUp from "../auth/sign-up/SignUp";
 import AppErrorSnackBar from "../../common/components/AppErrorSnackBar";
 import Profile from "../profile/Profile";
 import Box from "@mui/material/Box";
 import {useAppSelector} from "../../common/hooks/useAppSelector";
-import {getAppStatus} from "../../common/selectors/app-selectors";
-import {useEffect} from "react";
+import {checkIsAppInitialize} from "../../common/selectors/app-selectors";
 import {useAppDispatch} from "../../common/hooks/useAppDispatch";
-import {authMeTC} from "../auth/auth-slice";
-import {authAPI} from "../auth/auth-api";
+import Header from "../main/header/Header";
+import TripSearch from "../main/trip-search/TripSearch";
+import Information from "../main/information/Information";
+import Footer from "../main/footer/Footer";
+import {initializeAppTC} from "./appSlice";
+import {VWCircular} from "../../common/components/CircularProgress";
 
 export const App = () => {
 
-    const appStatus = useAppSelector(getAppStatus)
     const dispatch = useAppDispatch()
+    const isInitialize = useAppSelector(checkIsAppInitialize)
 
-
-    useEffect(()=>{
-        dispatch(authMeTC())
+   useEffect(()=>{
+        dispatch(initializeAppTC())
     },[])
 
-    const someFunc = () =>{
-        authAPI.refresh()
-    }
     return (
 
+
         <Container fixed>
-            <button onClick={someFunc}> click here to disturb</button>
-            {appStatus==="loading"&&<Box sx={{ width: '100%' }}>
-                <LinearProgress />
-            </Box>}
-            <Routes>
-                <Route index element={<Main/>}/>
-                <Route path={'login'} element={<SignIn/>}/>
-                <Route path={'register'} element={<SignUp/>}/>
-                <Route path={'profile'} element={<Profile/>}/>
-            </Routes>
-            <AppErrorSnackBar/>
+
+            <Box sx={{
+                margin:0,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                bgcolor: '#f1f1f1',
+                minHeight: '100vh'
+            }}>
+
+                {!isInitialize ?
+                    <Box sx={{alignSelf:"center"}}><VWCircular/></Box>
+                    : <>
+                        <Header/>
+
+                        <Routes>
+                            <Route index element={<TripSearch/>}/>
+                            <Route path={'login'} element={<SignIn/>}/>
+                            <Route path={'register'} element={<SignUp/>}/>
+                            <Route path={'profile'} element={<Profile/>}/>
+                        </Routes>
+
+                        <Information/>
+
+                        <Footer/>
+                    </>
+                }
+                <AppErrorSnackBar/>
+            </Box>
+
         </Container>
     );
 }
