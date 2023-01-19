2eb4dcbe9ea5e21ccb6762c362cdf139acd17511
alex <arhontes@yandex.ru>
2023-01-19 17:48:19 +0300

    refactor(Main.ts): change component

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -1,28 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Box} from "@mui/material";
-import TripSearch from "./trip-search/TripSearch";
-import Information from "./information/Information";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
-import DialogAuth from "../auth/DialogAuth";
+import {TripSearch} from "./trip-search/TripSearch";
+import {useNavigate} from "react-router-dom";
 
 const Main = () => {
 
-
+    const navigate = useNavigate()
 
     return (
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            bgcolor: '#cfe8fc',
-            height: '100vh'
-        }}>
-            <Header/>
-            <TripSearch/>
-            <Information/>
-            <Footer/>
-
+        <Box sx={{}}>
+            <TripSearch navigateOption={() => navigate("searchresults")}/>
         </Box>
     );
 };
